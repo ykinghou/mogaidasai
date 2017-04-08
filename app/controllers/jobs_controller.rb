efab64684070101499a893e02eb8ec33a7680caf
yking,hou <hyking@hotmail.com>
2017-04-08 17:44:51 +0800

    add user_id to job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create, :update, :edit, :destroy]
 
    def index
      @jobs = Job.all
@@ -20,6 +20,7 @@ class JobsController < ApplicationController
 
    def create
      @job = Job.new(job_params)
+     @job.user = current_user
      if @job.save
        redirect_to jobs_path
      else
