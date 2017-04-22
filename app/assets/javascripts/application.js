2f7117d60ef706e9c52f6fd270e6872d2e6e294d
yking,hou <hyking@hotmail.com>
2017-04-22 18:13:40 +0800

    调节welcome、边bar、注册登录页面

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,18 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+/* 向上按钮 */
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
